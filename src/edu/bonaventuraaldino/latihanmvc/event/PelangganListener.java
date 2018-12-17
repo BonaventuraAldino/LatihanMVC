bd1209c1f1f02cd973ba84234c7cd3b103da3a8c
BonaventuraAldino <aldysenda99@gmail.com>
2018-12-18 04:55:05 +0700

    Latihan MVC

---
+++ src/edu/bonaventuraaldino/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.bonaventuraaldino.latihanmvc.event;
+
+import edu.bonaventuraaldino.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author Aldy Senda
+ * Nama  : Bonaventura Aldino Senda Seni
+ * NIM   : 10117122
+ * Kelas : IF-3
+ */
+public interface PelangganListener {
+    
+    public void onChange (PelangganModel pelanggan);
+    
+}
+    
---
+++ src/edu/bonaventuraaldino/latihanmvc/model/PelangganModel.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.bonaventuraaldino.latihanmvc.model;
+
+import edu.bonaventuraaldino.latihanmvc.event.PelangganListener;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aldy Senda
+ * Nama  : Bonaventura Aldino Senda Seni
+ * NIM   : 10117122
+ * Kelas : IF-3
+ */
+public class PelangganModel {
+    
+    private String nama;
+    private String email;
+    private String noTelp;
+    
+    private PelangganListener pelangganlistener;
+
+    public PelangganListener getPelangganlistener() {
+        return pelangganlistener;
+    }
+
+    public void setPelangganlistener(PelangganListener pelangganlistener) {
+        this.pelangganlistener = pelangganlistener;
+    }
+    
+    
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+        fireOnChange();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        fireOnChange();
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+        fireOnChange();
+    }
+    
+    protected void fireOnChange(){
+        
+        if (pelangganlistener!=null) {
+            pelangganlistener.onChange(this);
+        }
+        
+    } 
+    
+    public void resetForm(){
+        setNama("");
+        setEmail("");
+        setNoTelp("");
+    }
+    
+    public void simpanForm (){
+        JOptionPane.showMessageDialog(null, "Berhasil Disimpan");
+    }
+    
+}
